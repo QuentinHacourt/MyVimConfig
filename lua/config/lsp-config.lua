3c933cc0eab00585623929eeb5a2898ad330f155
QuentinHacourt <quentin.hacourt@gmail.com>
2023-04-28 20:18:10 +0200

    changed some keymappings

--- lua/config/lsp-config.lua
+++ lua/config/lsp-config.lua
@@ -13,19 +13,19 @@ vim.api.nvim_create_autocmd('LspAttach', {
         local opts = { buffer = true }
 
         vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-        vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+        vim.keymap.set('n', '<leader>d', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+        vim.keymap.set('n', '<leader>D', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+        vim.keymap.set('n', '<leader>i', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+        vim.keymap.set('n', '<leader>o', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+        vim.keymap.set('n', '<leader>rf', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+        vim.keymap.set('n', '<leader>s', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+        vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+        vim.keymap.set({ 'n', 'x' }, '<leader>fo', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
         vim.keymap.set('n', '<leader>a', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
 
         vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
-        vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
-        vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
+        vim.keymap.set('n', '<leader>pe', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
+        vim.keymap.set('n', '<leader>ne', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
     end
 })
 
--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -1,6 +1,6 @@
 local utils = require('utils')
-require'nvim-tree'.setup {}
+require 'nvim-tree'.setup {}
 
-utils.map('n', '<C-n>', ':NvimTreeToggle<CR>')  -- Git status
-utils.map('n', '<leader>r', ':NvimTreeRefresh<CR>')  -- Git status
-utils.map('n', '<leader>n', ':NvimTreeFindFile<CR>')  -- Git status
+utils.map('n', '<C-n>', ':NvimTreeToggle<CR>')
+utils.map('n', '<leader>tr', ':NvimTreeRefresh<CR>')
+utils.map('n', '<leader>n', ':NvimTreeFindFile<CR>')
